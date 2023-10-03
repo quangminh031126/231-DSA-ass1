63503d0f6666a208eeb8ba371c5a6194c0edef83
Qmi <minh.pham1126@gmail.com>
2023-10-03 15:24:28 +0700

    format

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -2,36 +2,36 @@
 
 class imp_res : public Restaurant
 {
-	public:	
-		imp_res() {};
+public:
+	imp_res(){};
 
-		void RED(string name, int energy)
-		{
-			cout << name << " " << energy << endl;
-			customer *cus = new customer (name, energy, nullptr, nullptr);
-		}
-		void BLUE(int num)
-		{
-			cout << "blue "<< num << endl;
-		}
-		void PURPLE()
-		{
-			cout << "purple"<< endl;
-		}
-		void REVERSAL()
-		{
-			cout << "reversal" << endl;
-		}
-		void UNLIMITED_VOID()
-		{
-			cout << "unlimited_void" << endl;
-		}
-		void DOMAIN_EXPANSION()
-		{
-			cout << "domain_expansion" << endl;
-		}
-		void LIGHT(int num)
-		{
-			cout << "light " << num << endl;
-		}
+	void RED(string name, int energy)
+	{
+		cout << name << " " << energy << endl;
+		customer *cus = new customer(name, energy, nullptr, nullptr);
+	}
+	void BLUE(int num)
+	{
+		cout << "blue " << num << endl;
+	}
+	void PURPLE()
+	{
+		cout << "purple" << endl;
+	}
+	void REVERSAL()
+	{
+		cout << "reversal" << endl;
+	}
+	void UNLIMITED_VOID()
+	{
+		cout << "unlimited_void" << endl;
+	}
+	void DOMAIN_EXPANSION()
+	{
+		cout << "domain_expansion" << endl;
+	}
+	void LIGHT(int num)
+	{
+		cout << "light " << num << endl;
+	}
 };
