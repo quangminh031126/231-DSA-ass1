d6f1c0bc937f284b829aee5d4a410e592a7c784a
Qmi <minh.pham1126@gmail.com>
2023-10-04 17:46:08 +0700

    new class: cirCustomer to implement circular linked list

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -2,13 +2,45 @@
 
 class imp_res : public Restaurant
 {
+private:
+	class cirCustomer : public customer
+	{
+	public:
+		cirCustomer(string na, int e, cirCustomer *p, cirCustomer *ne) : customer(na, e, p, ne) {}
+		cirCustomer(cirCustomer *p = NULL, cirCustomer *ne = NULL)
+		{
+			this->prev = p;
+			this->next = ne;
+		}
+		~cirCustomer()
+		{
+			// Unlinking the nodes before
+			if (this->next == this)
+			{
+				this->next = nullptr;
+				this->prev = nullptr;
+			}
+			else
+			{
+				this->prev->next = this->next;
+				this->next->prev = this->prev;
+
+				this->next = nullptr;
+				this->prev = nullptr;
+			}
+		}
+	};
+
 public:
-	customer *recent;
+	cirCustomer *recent;
 	int count;
 	int cap;
 
 	imp_res() : recent(nullptr), count(0), cap(MAXSIZE){};
-
+	
+	void insert(const string& name, const int& energy){
+		
+	}
 	void RED(string name, int energy)
 	{
 		cout << name << " " << energy << endl;
