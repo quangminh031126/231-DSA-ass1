c2c7347d6e34754a034d3504500005f3f3c5cc57
Qmi <minh.pham1126@gmail.com>
2023-10-14 18:06:55 +0700

    feat: implement RED, isNameInRes, waitListQueue

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -30,10 +30,43 @@ private:
 			return this->energy - c->energy;
 		}
 	};
+	class waitListQueue
+	{
+	public:
+		Customer *front;
+		Customer *rear;
+		int count;
+		int cap;
+		waitListQueue()
+		{
+			cout << "waitList constructor called";
+		}
+		~waitListQueue() {}
+		bool isEmpty()
+		{
+			return count == 0;
+		}
+		int size()
+		{
+			return count;
+		}
+		void clear()
+		{
+			cout << "waitList clear called";
+		}
+		void insertRear()
+		{
+		}
+		void insertFront() {}
+		void removeRear() {}
+		void removeFront() {}
+		void sortWaitList() {}
+	};
 
 public:
 	Customer *head;
 	Customer *recent;
+	waitListQueue *WL;
 	int count;
 	int cap;
 	int timer;
@@ -47,6 +80,32 @@ public:
 	bool isEmpty()
 	{
 		return count == 0 && head == nullptr;
+	}
+
+	bool isNameInRes(const string &name)
+	{
+		if (!isEmpty())
+			return false;
+		Customer *curr = head;
+		do
+		{
+			if (curr->name == name)
+			{
+				return true;
+			}
+			// end of loop
+			curr = (Customer *)curr->next;
+		} while (curr != head);
+		return false;
+	}
+
+	bool isNameInWL(const string &name)
+	{
+	}
+
+	bool isNameHere(const string &name)
+	{
+		return isNameInRes(name) && isNameInWL(name);
 	}
 
 	void insert(const string &name, const int &energy) // Need to check cap before using
@@ -129,8 +188,69 @@ public:
 	}
 	void RED(string name, int energy)
 	{
-		cout << name << " " << energy << endl;
-		customer *cus = new customer(name, energy, nullptr, nullptr);
+		// cout << name << " " << energy << endl;
+		// customer *cus = new customer(name, energy, nullptr, nullptr);
+		if (energy == 0 || isNameHere(name))
+		{
+			return;
+		}
+		int MAXSIZE_2 = MAXSIZE / 2;
+		if (count < 0 || count > MAXSIZE || WL->count < 0 || WL->count > MAXSIZE)
+		{
+			cout << "out of range"
+				 << "\n";
+			return;
+		}
+		if (0 <= count && count < MAXSIZE_2)
+		{
+			if (energy >= recent->energy)
+			{
+				insertNext(name, energy);
+			}
+			else
+			{
+				insertPrev(name, energy);
+			}
+		}
+		else if (MAXSIZE_2 <= count && count < MAXSIZE)
+		{
+			int max_diff = 0;
+			int curr_diff = 0;
+			Customer *begin = recent;
+			Customer *curr = recent;
+			do
+			{
+				// do this during each loop
+				curr_diff = energy - curr->energy;
+				if (abs(curr_diff) > abs(max_diff))
+				{
+					recent = curr;
+					max_diff = curr_diff;
+				}
+				// end of each loop
+				curr = (Customer *)curr->next;
+			} while (curr != begin);
+
+			if (curr_diff < 0)
+			{
+				insertPrev(name, energy);
+			}
+			else
+			{
+				insertNext(name, energy);
+			}
+		}
+		else // count == MAXSIZE
+		{
+			if (WL->count < MAXSIZE)
+			{
+				WL->insertRear();
+			}
+			else
+			{
+				return;
+			}
+		}
 	}
 	void BLUE(int num)
 	{
